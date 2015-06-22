7b1417d0379421eeef5da99ce58be6b73e4c3e75
Walker de Alencar <walkeralencar@gmail.com>
2015-06-21 22:30:53 -0300

    ignoring provision.log and configured locales, closes #8

--- provision/setup.sh
+++ provision/setup.sh
@@ -4,6 +4,14 @@
 HOSTNAME="dev4php.local"
 
 echo "Development EnVironment for PHP v0.1"
-echo "                      based on WAS4P"
+echo "          [DEV4PHP] is based on WAS4P"
 
 hostname $HOSTNAME >> /vagrant/provision.log
+
+echo "[dev4php provisioning] Pre-configuring..."
+export DEBIAN_FRONTEND=noninteractive
+export LANGUAGE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+locale-gen en_US.UTF-8
+dpkg-reconfigure locales

32a12fb6a8461aaf1f1a6eafcd1692be16a7737c
Walker de Alencar <walkeralencar@gmail.com>
2015-06-22 02:47:39 -0300

    update echo informations to dev4php

--- provision/setup.sh
+++ provision/setup.sh
@@ -16,7 +16,7 @@ export LC_ALL=en_US.UTF-8
 locale-gen en_US.UTF-8
 dpkg-reconfigure locales
 
-echo "[vagrant provisioning] Configuring SWAP..."
+echo "[dev4php provisioning] Configuring SWAP..."
 grep -q "swapfile" /etc/fstab
 if [ $? -ne 0 ]; then
 fallocate -l 1024M /swapfile >> /vagrant/provision.log
@@ -30,21 +30,21 @@ echo "[dev4php provisioning] Updating & Upgrading OS..."
 apt-get update  >> /vagrant/provision.log
 apt-get upgrade -y  >> /vagrant/provision.log
 
-echo -n "[vagrant provisioning] Installing postfix, mailutils..."
+echo -n "[dev4php provisioning] Installing postfix, mailutils..."
 debconf-set-selections <<< "postfix postfix/mailname string $HOSTNAME"
 debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
 apt-get install -y postfix >> /vagrant/provision.log
 service postfix reload >> /vagrant/provision.log
 
-echo "[vagrant provisioning] Installing Git..."
+echo "[dev4php provisioning] Installing Git..."
 apt-get install -y git git-core >> /vagrant/provision.log
 
-echo "[vagrant provisioning] Installing PHP..."
+echo "[dev4php provisioning] Installing PHP..."
 apt-get install -y php5-common php5-cli php5-fpm >> /vagrant/provision.log
 
-echo "[vagrant provisioning] Installing PHP Extensions..."
+echo "[dev4php provisioning] Installing PHP Extensions..."
 apt-get install -y curl php5-curl php5-gd php5-imagick php5-intl php5-mcrypt php5-xdebug php5-xmlrpc >> /vagrant/provision.log
 
-echo "[vagrant provisioning] Installing Composer..."
+echo "[dev4php provisioning] Installing Composer..."
 curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
